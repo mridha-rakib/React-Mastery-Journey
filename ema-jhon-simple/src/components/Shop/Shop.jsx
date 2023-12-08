e0c2e4020f676c7af4ecf159eba2ad98fcecebe4
RAKIB <rakibmahmudmridha@gmail.com>
2023-12-08 19:33:43 +0600

    emajhon simple project

--- ema-jhon-simple/src/components/Shop/Shop.jsx
+++ ema-jhon-simple/src/components/Shop/Shop.jsx
@@ -1,9 +1,26 @@
 // import React from "react";
+import { useEffect, useState } from "react";
 import "./Shop.css";
+import Product from "../Product/Product";
+
 const Shop = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json"
+    )
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
   return (
     <div className="shop-container">
-      <div className="products-container">products comming here</div>
+      <div className="products-container">
+        {products.map((product) => (
+          <Product key={product.id} product={product}>
+            {" "}
+          </Product>
+        ))}
+      </div>
       <div className="cart-container">Order summary</div>
     </div>
   );
