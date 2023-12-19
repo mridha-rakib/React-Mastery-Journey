9f8d077ceb7837b93fbf86768f147893eac3eaba
RAKIB <rakibmahmudmridha@gmail.com>
2023-12-19 20:14:16 +0600

    Higer order function ReactJS

---
+++ higer-order-render-props/src/App.js
@@ -0,0 +1,14 @@
+import ClickCounter from "./Components/ClickCounter";
+import HoverCounter from "./Components/HoverCounter";
+// import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <ClickCounter />
+      <HoverCounter />
+    </div>
+  );
+}
+
+export default App;
